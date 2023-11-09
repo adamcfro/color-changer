2f72a68552f5fdef66aa2bd0e8de8d1b6b3f91e5
Adam Froehlich <adamcfro@gmail.com>
2023-11-09 09:05:13 -0800

    feat(init): initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * Randomly changes the container's background color and displays the rgb value.
+ *
+ * @format
+ * @param {Event} e - The event parameter.
+ */
+
+function changeColor(e) {
+  e.preventDefault();
+
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const container = document.querySelector(".container");
+  container.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+
+  const btn = document.querySelector(".btn");
+  btn.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+
+  const header = document.querySelector(".header");
+  header.innerHTML = `Color: rgb(${r},${g},${b})`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", changeColor);
+});
